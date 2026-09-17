d1f4feb26b3a230b16cc1ddaf3144ddf9c58a8f2
Andy Brody <git@abrody.com>
2026-09-17 13:58:14 -0400

    Fix SKU bug in pricing fetch script.
    The instance-pricing.sh script previously did not filter for particular
    SKUs like Linux no-OS multi-tenant.
    
    This meant that pricing data would arbitrarily pick whatever SKU
    happened to come last in the list, leading to incorrect results.
    
    For example, the c7gn.large data showed a price of $0.1811/hr ($132/mo),
    which matches the price for the SUSE Linux Enterprise Server (SLES) SKU,
    not the generic Linux SKU, which actually costs $0.1248/hr ($91/mo).
    
    Also fix the shebangs.

--- docs/pricing_analysis/instance-networking.sh
+++ docs/pricing_analysis/instance-networking.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env /bin/bash
+#!/usr/bin/env bash
 aws ec2 describe-instance-types \
+    --region us-east-1 \
     --output json \
     | jq '.InstanceTypes[] | { (.InstanceType): { vcpus: .VCpuInfo.DefaultVCpus, baseline: .NetworkInfo.NetworkCards[0].BaselineBandwidthInGbps, burst: .NetworkInfo.NetworkPerformance}}' \
     | jq -s add > instance-networking.json
--- docs/pricing_analysis/instance-pricing.sh
+++ docs/pricing_analysis/instance-pricing.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env /bin/bash
+#!/usr/bin/env bash
 aws pricing get-products \
     --service-code AmazonEC2 \
-    --filters "Type=TERM_MATCH,Field=location,Value=US East (N. Virginia)" \
+    --filters \
+        "Type=TERM_MATCH,Field=location,Value=US East (N. Virginia)" \
+        "Type=TERM_MATCH,Field=operatingSystem,Value=Linux" \
+        "Type=TERM_MATCH,Field=tenancy,Value=Shared" \
+        "Type=TERM_MATCH,Field=preInstalledSw,Value=NA" \
+        "Type=TERM_MATCH,Field=capacitystatus,Value=Used" \
+        "Type=TERM_MATCH,Field=operation,Value=RunInstances" \
     --region us-east-1 \
     | jq -rc '.PriceList[]' \
     | jq -rc 'select(.product.productFamily=="Compute Instance")' \
     | jq -rc '{ (.product.attributes.instanceType): { price: (.terms.OnDemand[].priceDimensions[].pricePerUnit.USD | tonumber) }}' \
-    | jq -s add > instance-pricing.json
+    | jq -sS add > instance-pricing.json
