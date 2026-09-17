581f71a33f563336719303a9ee426d5912fab334
Andy Brody <git@abrody.com>
2026-09-17 17:11:15 -0400

    Add script to find best option for given Gbps.

---
+++ docs/pricing_analysis/best-for-bandwidth.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ $# -lt 1 ]; then
+    echo >&2 "usage: $0 BANDWIDTH [LIMIT]"
+    echo >&2
+    echo >&2 "Find the best LIMIT instance type(s) for fck-nat with max_egress bandwidth >= BANDWIDTH (Gbps)"
+    exit 1
+fi
+
+bandwidth="$1"
+limit="${2-1}"
+
+jq --argjson egress "$bandwidth" --argjson limit "$limit" \
+    "to_entries | map(select(.value.max_egress >= \$egress)) | map(.value.price_monthly_per_gbps = ((.value.price_monthly / .value.max_egress * 100 | round) / 100)) | sort_by(.value.price) | .[:\$limit] | from_entries" instance-merged.json
