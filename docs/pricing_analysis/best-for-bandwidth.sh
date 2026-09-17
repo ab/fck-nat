6e6c1fd820b3714753182862cdbc727138044555
Andy Brody <git@abrody.com>
2026-09-17 17:43:22 -0400

    best-for-bandwidth.sh: json relative to script.

--- docs/pricing_analysis/best-for-bandwidth.sh
+++ docs/pricing_analysis/best-for-bandwidth.sh
@@ -12,4 +12,4 @@ bandwidth="$1"
 limit="${2-1}"
 
 jq --argjson egress "$bandwidth" --argjson limit "$limit" \
-    "to_entries | map(select(.value.max_egress >= \$egress)) | map(.value.price_monthly_per_gbps = ((.value.price_monthly / .value.max_egress * 100 | round) / 100)) | sort_by(.value.price) | .[:\$limit] | from_entries" instance-merged.json
+    "to_entries | map(select(.value.max_egress >= \$egress)) | map(.value.price_monthly_per_gbps = ((.value.price_monthly / .value.max_egress * 100 | round) / 100)) | sort_by(.value.price) | .[:\$limit] | from_entries" "$(dirname "$0")"/instance-merged.json
